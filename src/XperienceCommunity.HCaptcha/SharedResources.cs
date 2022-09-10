7bc94e9dd31efe377119116305658dbe6b9e59a8
Sean Wright <sean@wiredviews.com>
2022-09-10 15:42:05 -0400

    fix(HCaptcha): resource file

---
+++ src/XperienceCommunity.HCaptcha/SharedResources.cs
@@ -0,0 +1,12 @@
+using Kentico.Content.Web.Mvc;
+using XperienceCommunity.HCaptcha;
+
+[assembly: RegisterPageBuilderLocalizationResource(typeof(SharedResources), "en-us")]
+
+namespace XperienceCommunity.HCaptcha
+{
+    public class SharedResources
+    {
+
+    }
+}
